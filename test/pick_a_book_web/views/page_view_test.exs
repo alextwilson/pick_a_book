32dee6f3c641d6f7a121ceab2446b564f8973c5a
Agata Golebiewska <agata.golebiewska@yahoo.co.uk>
2018-03-06 11:45:41 +0000

    Adds test for index view
    Co-authored-by: GiadaSimonetti
    <name@example.com>21024993+GiadaSimonetti@users.noreply.github.com>
    Co-authored-by: AlexandraGF
    <29000357+AlexandraGF@users.noreply.github.com>
    Co-authored-by: alextwilson <github@alextwilson.co.uk>
    Co-authored-by: lewmoore <lewis.moore2707@gmail.com>
    Co-authored-by: bpourian <b.pourian@hotmail.co.uk>

--- test/pick_a_book_web/views/page_view_test.exs
+++ test/pick_a_book_web/views/page_view_test.exs
@@ -1,3 +1,9 @@
 defmodule PickABookWeb.PageViewTest do
   use PickABookWeb.ConnCase, async: true
+  import Phoenix.View
+
+  test "renders index.html" do
+    assert render_to_string(PickABookWeb.PageView, "index.html", []) =~
+           "Welcome to PickABook!"
+  end
 end
