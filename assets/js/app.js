9cad50c55d5be3ae5d0514c340ba8464f706afa4
Lewis Moore <lewis.moore2707@gmail.com>
2018-03-07 18:09:31 +0000

    Add component to index page

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,22 @@ ReactDOM.render(
   <HelloReact/>,
   document.getElementById("hello-react")
 )
+
+class NewBookForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <label>Title: <input type="text" title="title" /></label><br></br>
+        <label> Author: <input type="text" author="author" /></label><br></br>
+        <label> Genre: <input type="text" genre="genre" /></label><br></br>
+        <label> Description: <input type="text" description="description" /></label>
+        <input type="submit" value="Submit" />
+      </form>
+      )
+  }
+}
+
+ReactDOM.render(
+  <NewBookForm />,
+  document.getElementById("book-form")
+)
